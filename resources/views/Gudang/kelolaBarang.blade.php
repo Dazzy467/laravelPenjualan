9c169a7ee2fc44bf4aefd3b028251cbc6b7942d6
David Yusuf <davidyusufwaskito@gmail.com>
2023-11-17 20:48:03 +0700

    Card kelola barang

--- resources/views/Gudang/kelolaBarang.blade.php
+++ resources/views/Gudang/kelolaBarang.blade.php
@@ -3,58 +3,68 @@
     @include('layouts.sidebar-gudang')
 @endsection
 @section('content')
-<div>
-    @if (session('success'))
-    <div class="alert alert-success">
-        {{session('success')}}
-    </div>
-    @elseif (session('error'))
-        <div class="alert alert-danger">
-            {{session('error')}}
-        </div>
-    @endif
-    <!-- Simplicity is the essence of happiness. - Cedric Bledsoe -->
-    <table id="barangTable" class="table table-striped table-hover" style="width: 100%;">
-        <thead class="table-dark">
-            <tr>
-                <th scope="col" class="text-center">No</th>
-                <th scope="col">Barang</th>
-                <th scope="col">Stok</th>
-                <th scope="col">Harga</th>
-                <th scope="col" class="text-center">Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            @php
-                $no = 1;
-            @endphp
-            @foreach ($Barang as $val)
-            <tr>
-                <th scope="row" class="text-center">{{$no}}</th>
-                <td>{{$val->namaBarang}}</td>
-                <td>{{$val->stokBarang}}</td>
-                <td>{{$val->hargaBarang}}</td>
 
-                <td>
-                    <div class="d-flex justify-content-center">
-                        <div>
-                            <a href="{{__('EditBarang/'.$val->idBarang)}}" class="fa-solid fa-pen-to-square pe-2 text-primary" style="font-size: 24px;"></a>
-                            <a href="{{__('DeleteBarang/'.$val->idBarang)}}" class="fa-solid fa-trash-can text-danger" style="font-size: 24px;"></a>
-                        </div>
-                        
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="card bg-white shadow">
+            <div class="text-center pt-2">
+                <h3>Kelola Barang</h3>
+            </div>
+            <div class="card-body">
+                @if (session('success'))
+                <div class="alert alert-success">
+                    {{session('success')}}
+                </div>
+                @elseif (session('error'))
+                    <div class="alert alert-danger">
+                        {{session('error')}}
                     </div>
-                </td>
-            </tr>
-            @php
-                $no++;
-            @endphp
-            @endforeach
-        </tbody>
-    </table>
-    <div class="d-flex">
-        <a href="TambahBarangForm" class="btn btn-primary">
-            <span>Tambah Barang</span>
-        </a>
+                @endif
+                <!-- Simplicity is the essence of happiness. - Cedric Bledsoe -->
+                <table id="barangTable" class="table table-striped table-hover" style="width: 100%;">
+                    <thead class="table-dark">
+                        <tr>
+                            <th scope="col" class="text-center">No</th>
+                            <th scope="col">Barang</th>
+                            <th scope="col">Stok</th>
+                            <th scope="col">Harga</th>
+                            <th scope="col" class="text-center">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @php
+                            $no = 1;
+                        @endphp
+                        @foreach ($Barang as $val)
+                        <tr>
+                            <th scope="row" class="text-center">{{$no}}</th>
+                            <td>{{$val->namaBarang}}</td>
+                            <td>{{$val->stokBarang}}</td>
+                            <td>{{$val->hargaBarang}}</td>
+
+                            <td>
+                                <div class="d-flex justify-content-center">
+                                    <div>
+                                        <a href="{{__('EditBarang/'.$val->idBarang)}}" class="fa-solid fa-pen-to-square pe-2 text-primary" style="font-size: 24px;"></a>
+                                        <a href="{{__('DeleteBarang/'.$val->idBarang)}}" class="fa-solid fa-trash-can text-danger" style="font-size: 24px;"></a>
+                                    </div>
+                                    
+                                </div>
+                            </td>
+                        </tr>
+                        @php
+                            $no++;
+                        @endphp
+                        @endforeach
+                    </tbody>
+                </table>
+                <div class="d-flex">
+                    <a href="TambahBarangForm" class="btn btn-primary">
+                        <span>Tambah Barang</span>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
